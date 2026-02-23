b3c122cf8bf16b0c6c3d44eae9d94103b83f5d3a
Ananya SN <ananyasn37@gmail.com>
2026-02-24 00:01:02 +0530

    second

--- app/dashboardWrapper.tsx
+++ app/dashboardWrapper.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
 
-const DashboardWrapper = ({  children  } : {children: React.ReactNode}) => {
+const DashboardWrapper = ({  children  } : { children: React.ReactNode }) => {
   return (
     <div className="flex min-h-screen w-full bg-gray-50 text-gray-900"></div>
+
+    {         }
+    <main className={`dark:bg-dark-bg flex w-full flex-col bg-gray-50   md:pl-64`}>
+    </main>
   );
 };
 
