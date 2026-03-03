1ff1ac5f57a1f0d99dca47362ad7ae1beeab29e6
Ananya SN <ananyasn37@gmail.com>
2026-03-03 22:56:05 +0530

    dashboard recommit

--- app/dashboardWrapper.tsx
+++ app/dashboardWrapper.tsx
@@ -6,7 +6,8 @@ const DashboardWrapper = ({  children  } : { children: React.ReactNode }) => {
     <div className="flex min-h-screen w-full bg-gray-50 text-gray-900"></div>
 
     {         }
-    <main className={`dark:bg-dark-bg flex w-full flex-col bg-gray-50   md:pl-64`}>
+    <main className={`dark:bg-dark-bg flex w-full flex-col bg-gray-50 md:pl-64`}>
+      
     </main>
   );
 };
