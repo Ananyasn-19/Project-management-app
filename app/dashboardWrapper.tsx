934906118146de588516fd03d3517653cc873a29
Ananya SN <ananyasn37@gmail.com>
2026-03-04 23:55:07 +0530

    dashboard recommit

--- app/dashboardWrapper.tsx
+++ app/dashboardWrapper.tsx
@@ -1,14 +1,18 @@
-import React from 'react'
+// app/components/DashboardWrapper.tsx
+import React from "react";
 
+type DashboardWrapperProps = {
+  children: React.ReactNode;
+};
 
-const DashboardWrapper = ({  children  } : { children: React.ReactNode }) => {
+const DashboardWrapper = ({ children }: DashboardWrapperProps) => {
   return (
-    <div className="flex min-h-screen w-full bg-gray-50 text-gray-900"></div>
-
-    {         }
-    <main className={`dark:bg-dark-bg flex w-full flex-col bg-gray-50 md:pl-64`}>
-      
-    </main>
+    <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
+      {/* Sidebar would go here */}
+      <main className="dark:bg-black-bg flex w-full flex-col bg-gray-50 md:pl-64">
+        {children}
+      </main>
+    </div>
   );
 };
 
